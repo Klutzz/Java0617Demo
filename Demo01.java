7d31700ed3d03f2b0d0caa5cf2355418d115081c
Klutzz <15657303025@163.com>
2019-11-15 20:39:05 +0800

    更新了Demo

--- Demo01.java
+++ Demo01.java
@@ -1,8 +1,9 @@
-// package com.offcn;  这里修改了一下 2019/11/15 20:34
+// package com.offcn;  这里修改了一下 2019-11-15 20:34
 
 public class Demo01 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+		System.out.println("Welcome to Beijing!");  // 2019-11-15 20:36
     }
 }
